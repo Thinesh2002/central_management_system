55f447fc51385014c936de3954ed17579462f709
Thineshkaran <sthinesh0730@gmail.com>
2026-06-25 19:24:53 +0530

    s

--- backend/config/order_management_db/cm_order_management.js
+++ backend/config/order_management_db/cm_order_management.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const db = mysql.createPool({
-  host: process.env.ORDER_DB_HOST || process.env.DB_HOST,
-  user: process.env.ORDER_DB_USER || process.env.DB_USER,
-  password: process.env.ORDER_DB_PASSWORD || process.env.DB_PASSWORD,
-  database: process.env.ORDER_DB_NAME || "cm_order_management",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password:process.env.DB_PASSWORD,
+  database: process.env.ORDER_DB_NAME,
   waitForConnections: true,
   connectionLimit: Number(process.env.ORDER_DB_CONNECTION_LIMIT || 10),
   queueLimit: 0,
