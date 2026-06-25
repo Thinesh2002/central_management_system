c352f786aa005a1012b72e6d2cce03b624e1e2bf
Thineshkaran <sthinesh0730@gmail.com>
2026-06-25 19:26:43 +0530

    e

--- backend/config/marketplace_management_db/cm_marketplace_management.js
+++ backend/config/marketplace_management_db/cm_marketplace_management.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const marketplaceManagementDb = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASS || "",
-  database: process.env.MP_DB_NAME || "cm_marketplace_managemet",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.MP_DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
